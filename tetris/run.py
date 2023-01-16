67d05abd789b6d4f0fe7b5fed587ab8d60d5ea6d
mmm4707 <kim9905177@gmail.com>
2023-01-16 22:08:38 +0900

    add_menu

---
+++ tetris/run.py
@@ -0,0 +1,68 @@
+import pygame
+import pygame_menu
+from Tetris import *
+import time
+
+class run:
+
+ pygame.init()
+
+white = (255, 255, 255)
+
+titleImg = pygame.image.load('assets/images/icon.png')
+startImg = pygame.image.load('assets/images/start.png')
+quitImg = pygame.image.load('assets/images/exit.png')
+clickStartImg = pygame.image.load('assets/images/start2.png')
+clickQuitImg = pygame.image.load('assets/images/exit2.png')
+
+
+display_width = 350
+display_height = 450
+gameDisplay = pygame.display.set_mode((display_width, display_height))
+pygame.display.set_caption("테트리스")
+
+clock = pygame.time.Clock()
+
+
+class Button:
+    def __init__(self, img_in, x, y, width, height, img_act, x_act, y_act, action = None):
+        mouse = pygame.mouse.get_pos()
+        click = pygame.mouse.get_pressed()
+        if x + width > mouse[0] > x and y + height > mouse[1] > y:
+            gameDisplay.blit(img_act,(x_act, y_act))
+            if click[0] and action != None:
+                time.sleep(1)
+                action()
+        else:
+            gameDisplay.blit(img_in,(x,y))
+
+def quitgame():
+    pygame.quit()
+    sys.exit()
+
+
+def mainmenu():
+
+    menu = True
+
+    while menu:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+        gameDisplay.fill(white)
+        
+        titletext = gameDisplay.blit(titleImg, (105,50))
+        startButton = Button(startImg,120,150,96,96,clickStartImg,120,150,start)
+        quitButton = Button(quitImg,120,350,96,96,clickQuitImg,120,350,quitgame)
+        pygame.display.update()
+        clock.tick(15)
+        
+def start():
+    Tetris().run()
+
+
+
+mainmenu()
+game_loop()
